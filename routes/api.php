bede43fac9d1e5dc7160035aa2e210da428a97cf
fcinque <fcinque@cnrt.gob.ar>
2023-04-25 09:15:09 -0300

    fix routes of api

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Api\v1\AuthController;
 
 Route::group([
     'prefix' => 'v1/auth'
-
 ], function ($router) {
     Route::post('login', [AuthController::class, 'login'])->name('login');
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
@@ -14,11 +13,14 @@ Route::group([
     Route::post('me', [AuthController::class, 'me'])->name('me');
 });
 
-Route::group(['middleware' => 'auth:api'], function () {
-    Route::get('v1/people', [ApiController::class, 'getPeople'])->name('getPeople');
-    Route::get('v1/people/{id}', [ApiController::class, 'getPeopleById'])->name('getPeopleById');
-    Route::get('v1/planets', [ApiController::class, 'getPlanets'])->name('getPlanets');
-    Route::get('v1/planets/{id}', [ApiController::class, 'getPlanetById'])->name('getPlanetById');
-    Route::get('v1/vehicles', [ApiController::class, 'getVehicles'])->name('getVehicles');
-    Route::get('v1/vehicles/{id}', [ApiController::class, 'getVehicleById'])->name('getVehicleById');
+Route::group([
+    'middleware' => 'auth:api',
+    'prefix' => 'v1'
+], function () {
+    Route::get('people', [ApiController::class, 'getPeople'])->name('getPeople');
+    Route::get('people/{id}', [ApiController::class, 'getPeopleById'])->name('getPeopleById');
+    Route::get('planets', [ApiController::class, 'getPlanets'])->name('getPlanets');
+    Route::get('planets/{id}', [ApiController::class, 'getPlanetById'])->name('getPlanetById');
+    Route::get('vehicles', [ApiController::class, 'getVehicles'])->name('getVehicles');
+    Route::get('vehicles/{id}', [ApiController::class, 'getVehicleById'])->name('getVehicleById');
 });
